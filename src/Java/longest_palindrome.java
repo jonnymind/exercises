0eccf5aedf4ebca8dbe7fdec3db0f31f6c727b38
Giancarlo Niccolai <Giancarlo Niccolai>
2022-01-06 14:11:10 +0000

    Added a java section

---
+++ src/Java/longest_palindrome.java
@@ -0,0 +1,38 @@
+
+String FongestPalindrome(String word) 
+{
+    if(word.length() == 0)
+    return "";
+
+    char[] chars = word.toCharArray();
+    String candidate = "" + chars[0];
+
+    int beg = 0;
+    while(beg < chars.length-1) {
+    int end = chars.length-1;
+    while(end >= beg + candidate.length()) {
+        if(chars[beg] == chars[end]) {
+        int pbeg = beg+1;
+        int pend = end-1;
+        while(pbeg < pend) {
+            if(chars[pbeg] != chars[pend]) {
+            break;
+            }
+            pbeg ++;
+            pend --;
+        }
+        // Was this a palinrome?
+        if(pbeg >= pend) {
+            // We know for a fact the candidate is shorter.
+            candidate = word.substring(beg, end+1);
+            beg += candidate.length()-1; // the loop already increments.
+            break;
+        }
+        }
+        --end;
+    }
+    beg++;
+    }
+
+    return candidate;
+}
